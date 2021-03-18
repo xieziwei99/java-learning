b9c087300730c380362c0ca46bd21c0bc14d036d
xieziwei99 <1374884318@qq.com>
2021-03-18 17:08:11 +0800

    更改Stack的实现，用数组替换ArrayList

--- javacore/src/数据结构/Stack.java
+++ javacore/src/数据结构/Stack.java
@@ -1,38 +1,54 @@
 package 数据结构;
 
-import java.util.ArrayList;
+import java.lang.reflect.Array;
 
 /**
  * @author xzw
  * 2021-01-25
  */
 public class Stack<T> {
-    private final ArrayList<T> element = new ArrayList<>();
+    private int capacity = 100;
+    private T[] elements;
+    private int top = 0;
+
+    @SuppressWarnings("unchecked")
+    public Stack(Class<T> clazz) {
+        elements = (T[]) Array.newInstance(clazz, capacity);
+    }
 
     public boolean isEmpty() {
-        return this.element.isEmpty();
+        return top == 0;
     }
 
     public void push(T t) {
-        this.element.add(t);
+        if (top == capacity) {
+            @SuppressWarnings("unchecked")
+            T[] temp = (T[]) Array.newInstance(t.getClass(), capacity * 2);
+            System.arraycopy(elements, 0, temp, 0, capacity);
+            elements = temp;
+            capacity *= 2;
+        }
+        this.elements[top] = t;
+        top++;
     }
 
     public T peek() {
-        return this.isEmpty() ? null : this.element.get(element.size() - 1);
+        return this.isEmpty() ? null : elements[top - 1];
     }
 
     public void pop() {
         if (!this.isEmpty()) {
-            this.element.remove(element.size() - 1);
+            top--;
         }
     }
 
     public static void main(String[] args) {
-        Stack<Integer> integerStack = new Stack<>();
-        for (int i = 0; i < 10; i++) {
+        Stack<Integer> integerStack = new Stack<>(Integer.class);
+        for (int i = 0; i < 100; i++) {
             integerStack.push(i);
         }
-        for (int i = 0; i < 10; i++) {
+        integerStack.push(10000);
+        while (!integerStack.isEmpty()) {
             System.out.println(integerStack.peek());
             integerStack.pop();
         }
